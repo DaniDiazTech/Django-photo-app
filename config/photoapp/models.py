b27aeb434333bbe9a24ad3ed6d86247af41474f8
Daniel <danieldiazgonza52@gmail.com>
2021-05-15 23:06:57 -0500

    {Added} Django app

---
+++ config/photoapp/models.py
@@ -0,0 +1,21 @@
+'''Photoapp Models'''
+
+from django.db import models
+
+from django.contrib.auth import get_user_model
+
+from taggit.managers import TaggableManager
+
+class Photo(models.Model):
+    
+    title = models.CharField(max_length=45)
+    
+    description = models.CharField(max_length=250) 
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    image = models.ImageField(upload_to='photos/')
+
+    submitter = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    tags = TaggableManager() 
